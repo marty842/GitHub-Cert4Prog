7d6378146523d663214914b32b8240d9b9bee57a
marty842 <marty2407@gmail.com>
2014-03-27 11:49:48 +0800

    Add name project

---
+++ MartinNameProject/aNameProject/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstBoxName.Items.Add("Martin")
+        lstBoxName.Items.Add()
+
+    End Sub
+End Class
